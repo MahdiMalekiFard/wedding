02ca83ab3a4cc365bfb015abdf7e4e698adfb5b9
Mahdi_new <mmf1344py@gmail.com>
2025-09-30 09:13:14 +0330

    install flatpicker. update powergrid css and js.

--- config/livewire-powergrid.php
+++ config/livewire-powergrid.php
@@ -13,8 +13,8 @@ return [
     | Configure here the theme of your choice.
     */
 
-    'theme' => \PowerComponents\LivewirePowerGrid\Themes\Tailwind::class,
-    // 'theme' => \PowerComponents\LivewirePowerGrid\Themes\DaisyUI::class,
+//    'theme' => \PowerComponents\LivewirePowerGrid\Themes\Tailwind::class,
+     'theme' => \PowerComponents\LivewirePowerGrid\Themes\DaisyUI::class,
     // 'theme' => \PowerComponents\LivewirePowerGrid\Themes\Bootstrap5::class,
 
     'cache_ttl' => null,
@@ -96,7 +96,7 @@ return [
     |
     */
 
-    'filter' => 'inline',
+    'filter' => 'outside',
 
     /*
     |--------------------------------------------------------------------------
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,4 @@
 import './bootstrap';
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
+import 'flatpickr';
+import 'flatpickr/dist/l10n/fa.js';
