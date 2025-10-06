9a23dc1ef9d5e316799fdd2eca8c1497054a56b3
Mahdi_new <mmf1344py@gmail.com>
2025-10-06 13:02:14 +0330

    change the home video format to mp4.

--- resources/views/livewire/web/pages/home-page.blade.php
+++ resources/views/livewire/web/pages/home-page.blade.php
@@ -168,7 +168,7 @@
                         <div class="img-anim">
                             <img src="/assets/img/normal/video_1-2.png" alt="img">
                         </div>
-                        <a href="{{ asset('assets/vids/video.MOV') }}" class="play-btn popup-video background-image">
+                        <a href="{{ asset('assets/vids/video.mp4') }}" class="play-btn popup-video background-image">
                             <i class="fas fa-solid fa-play"></i>
                         </a>
                     </div>
